cee5a0b5f51c05c0f71f25c7b82086e62fb2bd91
SevincHasanova <127186047+SevincHasanova@users.noreply.github.com>
2023-04-26 09:09:27 +0400

    Add files via upload

---
+++ Yeni qovluq/main.js
@@ -0,0 +1,74 @@
+// // a. Toplama çıxma, toplama, vurma, bölməyə uyğun funksiya yaradirsiz 2 eded ve 1 operator('+','-','*','/') qebul edir. Qebul etdiyi operatora uygun a və b ədədləri üzərində hesablama aparib alinan deyeri qaytarır.
+
+
+// var toplama = (m, n) => m + n;
+// var plusMethod = function (m, n) {
+//     return m + n;
+// }
+// var p = toplama(10, 2);
+// alert(p);
+
+
+// var chixma = (m, n) => m - n;
+// var minusMethod = function (m, n) {
+//     return m - n;
+// }
+// var p = chixma = (10, 2);
+// alert(p);
+
+
+// function vurma(m, n) {
+//     var m, n;
+//     return m * n;
+// }
+// var u = vurma(10, 2);
+// alert(u);
+
+
+// function bolme(m, n) {
+//     var m, n;
+//     return m / n;
+// }
+// var u = bolme(10, 2);
+// alert(u);
+
+
+
+
+
+
+
+// // b.Methodlar yaradirsiz. Methoda ad gonderende adi ekrana cixardir(Sabir), ad ve soyad gonderende soyadi adi ekrana cixardir(Guliyev Sabir), ad,soyad ve ata adı göndərildikdə adin baş hərfi nöqtə soyad ata adi ekrana cixardirsiz(S.Guliyev Mehti). Methodlarin adi eyni olmalıdır.
+
+
+// prompt();
+
+// var people = [
+//     {
+//         name: "Sevinc",
+//         surname: "Hasanova",
+//         fatherName: "Elman",
+//     },
+
+//     {
+//         name: "Vugar",
+//         surname: "Hasanov",
+//         fatherName: "Elman",
+//     },
+
+//     {
+//         name: "Sattar",
+//         surname: "Hasanov",
+//         fatherName: "Elman",
+//     }
+// ];
+// for (var i = 0; i < people.length; i++)
+
+//     console.log(people[0, 1, 2].name);
+// console.log(people[0, 1, 2].surname);
+// console.log(people[0, 1, 2].fatherName);
+
+
+
+
+
